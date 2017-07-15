Fixed gradient descent algorithm

MachineLearning.py
@@ -1,35 +1,51 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-from matplotlib import style 
+from matplotlib import style
+from sklearn.metrics import mean_squared_error 
 
 def batch_gradient_descent(X, y):
 	numOfSamples = len(X)
 	numOfFeatures = X.shape[1] 
 	theta = np.matrix(np.zeros(numOfFeatures)).T
-	alpha = 0.00001
-	print(computeCost(X, y, theta))
+	alpha = 0.0001
+	#print(compute_cost(X, y, theta))
+	costList = [None] * 50
 	newTheta = theta
-	for iteration in range(1000):
-		errors = X.dot(theta) - y
+	for iteration in range(50):
+		errors = X.dot(theta).T - y
 		for featureNum in range(numOfFeatures):
 			partialDerivative = np.multiply(errors, X[:, featureNum])
 			newTheta[featureNum, 0] = theta[featureNum, 0] - alpha * 1/numOfSamples * partialDerivative.sum()
 		theta = newTheta
-	print(computeCost(X, y, theta))
+		costList[iteration] = compute_cost(X, y, theta)
+		alpha = 0.0001
+	return newTheta, costList
 
-	return newTheta
+def gradientDescent(X, y, theta):  
+    temp = np.matrix(np.zeros(theta.shape))
+    parameters = int(theta.ravel().shape[1])
+    cost = np.zeros(1000)
+
+    for i in range(1000):
+        error = (X * theta).T - y
+
+        for j in range(parameters):
+            term = np.multiply(error, X[:,j])
+            temp[j,0] = theta[j, 0] - ((0.0001 / len(X)) * np.sum(term))
+
+        theta = temp
+
+    return theta
 
 def compute_cost(X, y, theta):
-	cost = y - X.dot(theta)
-	return np.power(cost.sum(), 2)
-
-def computeCost(X, y, theta):  
-    inner = np.power(((X * theta) - y), 2)
-    return np.sum(inner) / (2 * len(X))
+	totalCost = X.dot(theta).T - y
+	squaredErrors = np.power(totalCost, 2)
+	meanSquaredError = squaredErrors.sum()/len(X)
+	return meanSquaredError
 
 def run():
-	data = pd.read_csv("data_file.csv", sep=",", header=0)
+	data = pd.read_csv("data_file.csv", sep=",", header=None)
 	X = data.ix[:, 0]
 	xArray = np.asarray(X)
 	X = np.column_stack((xArray, np.ones(len(X))))
@@ -37,21 +53,13 @@ def run():
 	y = data.ix[:, 1]
 	yArray = np.asarray(y)
 	y = np.matrix(y)
-	theta = batch_gradient_descent(X, y)
+	theta, costList = batch_gradient_descent(X, y)
+	print("cost: " + str(compute_cost(X, y, theta)))
 	plt.scatter(xArray, yArray)
 	x = np.arange(xArray.min(), xArray.max(), 1)
-	f = theta[0, 0] + x * theta[1, 0] 
-	plt.plot(x, f, 'r')
+	f = theta[1, 0] + x * theta[0, 0]
+	plt.plot(x, f, "r")
 	plt.show()
-	#trainingExample = np.array(points[0])
-	#parameters = linear_regression(points, [None] * points.shape[1])
-	#xValues = points[:, 0]
-	#yValues = points[:, 1]
-	#style.use('ggplot')
-	#plt.plot(xValues, yValues, 'r')
-	#plt.plot(xValues, parameters[0] + xValues * parameters[1], 'b')
-	#plt.show()
-
 
 if __name__ == '__main__':
 	run()