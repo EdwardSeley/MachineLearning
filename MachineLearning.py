cd6faa733aae1c9b4342d4c5cde1e2ebeaef24f2
Edward Seley <edwardseley@gmail.com>
2017-07-17 13:59:44 -0500

    Updated batch gradient descent

--- MachineLearning.py
+++ MachineLearning.py
@@ -2,41 +2,23 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from matplotlib import style
-from sklearn.metrics import mean_squared_error 
+from sklearn import datasets, linear_model
 
 def batch_gradient_descent(X, y):
 	numOfSamples = len(X)
 	numOfFeatures = X.shape[1] 
 	theta = np.matrix(np.zeros(numOfFeatures)).T
-	alpha = 0.0001
-	#print(compute_cost(X, y, theta))
-	costList = [None] * 50
+	alpha = 0.02
+	costList = [None] * 1000
 	newTheta = theta
-	for iteration in range(50):
-		errors = X.dot(theta).T - y
+	for iteration in range(1000):
+		errors = y - X.dot(theta).T
 		for featureNum in range(numOfFeatures):
-			partialDerivative = np.multiply(errors, X[:, featureNum])
-			newTheta[featureNum, 0] = theta[featureNum, 0] - alpha * 1/numOfSamples * partialDerivative.sum()
+			partialDerivatives = errors.dot(X[:, featureNum])
+			newTheta[featureNum, 0] = theta[featureNum, 0] + alpha * 1/numOfSamples * partialDerivatives
 		theta = newTheta
 		costList[iteration] = compute_cost(X, y, theta)
-		alpha = 0.0001
 	return newTheta, costList
-
-def gradientDescent(X, y, theta):  
-    temp = np.matrix(np.zeros(theta.shape))
-    parameters = int(theta.ravel().shape[1])
-    cost = np.zeros(1000)
-
-    for i in range(1000):
-        error = (X * theta).T - y
-
-        for j in range(parameters):
-            term = np.multiply(error, X[:,j])
-            temp[j,0] = theta[j, 0] - ((0.0001 / len(X)) * np.sum(term))
-
-        theta = temp
-
-    return theta
 
 def compute_cost(X, y, theta):
 	totalCost = X.dot(theta).T - y
@@ -46,6 +28,9 @@ def compute_cost(X, y, theta):
 
 def run():
 	data = pd.read_csv("data_file.csv", sep=",", header=None)
+	points = data.values.tolist()
+	#otherTheta = gradient_descent_runner(points, 0, 0, 0.01, 1000)
+	#newTheta = np.matrix([otherTheta[1], otherTheta[0]]).T
 	X = data.ix[:, 0]
 	xArray = np.asarray(X)
 	X = np.column_stack((xArray, np.ones(len(X))))
@@ -55,10 +40,19 @@ def run():
 	y = np.matrix(y)
 	theta, costList = batch_gradient_descent(X, y)
 	print("cost: " + str(compute_cost(X, y, theta)))
-	plt.scatter(xArray, yArray)
+	#print("cost: " + str(compute_cost(X, y, newTheta)))
 	x = np.arange(xArray.min(), xArray.max(), 1)
+
 	f = theta[1, 0] + x * theta[0, 0]
-	plt.plot(x, f, "r")
+	#plt.scatter(xArray, yArray)
+	#plt.plot(x, f, "r"
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.set_title("Gradient Descent")
+	ax.set_xlabel("Independent Variable")
+	ax.set_ylabel("Dependent Variable")
+	ax.scatter(xArray, yArray)
+	ax.plot(x, f, "r")
 	plt.show()
 
 if __name__ == '__main__':
